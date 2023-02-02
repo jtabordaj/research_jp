97b3ef2edd6e400d725220795efde2d88f8ae77a
jtabordaj <jtabordaj@uninorte.edu.co>
2023-02-02 12:43:06 -0500

    added comments for guidelines

--- desocupados.R
+++ desocupados.R
@@ -3,3 +3,8 @@ source("./dependencies.R")
 grabData("desocupados", ".csv2", 1, 12)
 allDataFrames <- names(which(unlist(eapply(.GlobalEnv,is.data.frame))))
 onlyDesocup <- allDataFrames[grep("^desocup", allDataFrames)]
+
+
+# Remember:
+# Revisar si esta/agregar p6050 en caracteristicas generales
+# Necesito dsi en modulo desocupados o una variable que de desempleo
