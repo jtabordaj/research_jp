faf87a3a96a7cd780e9378cc9ff25ad43bbdd0b5
jtabordaj <jtabordaj@uninorte.edu.co>
2023-02-06 13:40:54 -0500

    I think I stabilized desocup

--- paradata.R
+++ paradata.R
@@ -1,3 +1,7 @@
 joinCriteria <- c("DIRECTORIO", "SECUENCIA_P")
 
-paradata2012 <- inner_join(data2012, desocupados2012.xlsx, by = joinCriteria)
+paradata2012 <- left_join(data2012, desocupados2012.xlsx, by = joinCriteria)
+
+branch <- data2012
+
+branch <- branch[!branch$DIRECTORIO %in% paradata2012$DIRECTORIO & !branch$SECUENCIA_P %in% paradata2012$SECUENCIA_P, ]
