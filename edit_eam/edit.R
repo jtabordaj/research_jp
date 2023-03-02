3058a8c38c74daef7c5d8caeecc2d156c5622b37
jtabordaj <jtabordaj@uninorte.edu.co>
2023-03-02 13:52:29 -0500

    more mutates until human capital distribution ratios

--- edit_eam/edit.R
+++ edit_eam/edit.R
@@ -9,6 +9,7 @@ edit <- edit1718
 ### Mutates
 
 ## Tipo de innovacion
+# Numericas
 
 edit$I1R4C2[is.na(edit$I1R4C2)] <- 0
 edit <- edit %>% mutate(innProceso = I1R4C2) # numero de procesos
@@ -20,6 +21,7 @@ edit$I1R5C2[is.na(edit$I1R5C2)] <- 0
 edit <- edit %>% mutate(innOrganizacional = I1R5C2) # numero de organizacionales
 
 ## Impacto de la innovacion
+# Dummys
 
 edit$I2R1C1[is.na(edit$I2R1C1)] <- 3
 edit <- edit %>% mutate(mejoraBS = ifelse(I2R1C1 != 3, 1, 0)) #1 = Hay mejoras B/S
@@ -51,6 +53,7 @@ edit$I2R15C1[is.na(edit$I2R15C1)] <- 3
 edit <- edit %>% mutate(reduceCostoOtros = ifelse(I2R13C1 != 3 | I2R14C1 != 3 | I2R15C1 != 3, 1, 0)) #1 = Reduce otros costos (regulaciones, reciclaje, impuestos)
 
 ## Taxonomia de la innovacion
+# Numeric
 
 edit$I1R4C2N[is.na(edit$I1R4C2N)] <- 0
 edit <- edit %>% mutate(innRadical = I1R4C2N) # numero de radicales
@@ -59,6 +62,7 @@ edit$I1R4C2M[is.na(edit$I1R4C2M)] <- 0
 edit <- edit %>% mutate(innIncremental = I1R4C2M) # numero de incrementales
 
 ## Tipo de empresa innovadora
+# Dummys
 
 edit <- edit %>% mutate(innovadorAmplio = ifelse(TIPOLO == "AMPLIA", 1, 0)) #1 = Innovador amplio
 edit <- edit %>% mutate(noInnovadora = ifelse(TIPOLO == "NOINNO", 1, 0)) #1 = No innovadora
@@ -67,12 +71,14 @@ edit <- edit %>% mutate(innovadorPotencial = ifelse(TIPOLO == "POTENC", 1, 0)) #
 edit <- edit %>% mutate(otroTipo = ifelse(innovadorAmplio + noInnovadora + innovadorEstricto + innovadorPotencial == 0, 1, 0)) #1 = Otro tipo
 
 ## Uso de TICs
+# Dummy
 
 edit$II1R4C1[is.na(edit$II1R4C1)] <- 0
 edit$II1R4C2[is.na(edit$II1R4C2)] <- 0
 edit <- edit %>% mutate(usaTICs = ifelse(II1R4C1 > 0 | II1R4C2 > 0, 1,0)) #1 = Gasta en TICs
 
 ## Networking de innovacion
+# Dummys
 
 edit$V3R3C1[is.na(edit$V3R3C1)] <- 0
 edit$V3R5C1[is.na(edit$V3R5C1)] <- 0
@@ -103,6 +109,7 @@ edit <- edit %>% mutate(sinNetwork = ifelse(modificaOtros + networkEmpresas + ne
 # Cuando hagas dicc revisa las organizaciones de cada coso
 
 ## Personal ocupado
+# Numerics + Categories
 
 edit <- edit %>% mutate(totOcupados2017 = IV1R11C1)
 edit <- edit %>% mutate(totOcupados2018 = IV1R11C2)
@@ -120,11 +127,13 @@ edit <- edit %>% mutate(catOcupados2018 = ifelse(IV1R11C2 <= 10, 1,
 ) # 1 = Menos de 10. 2 = Entre 10 y 50. 3 = Entre 50 y 200. 4 = Mas de 200 
 
 ## CIIU
+# Categories, pero numericas
 
 colnames(edit)[2] <- "ciiu4"
 edit <- edit %>% mutate(ciiu4Digitos = ciiu4)
 
 ## Personal en ACTI
+# Numerics
 
 edit$IV1R11C3[is.na(edit$IV1R11C3)] <- 0
 edit$IV1R11C4[is.na(edit$IV1R11C4)] <- 0
@@ -138,4 +147,70 @@ edit <- edit %>% mutate(ratioActi2018 = ocupadosACTI2018/totOcupados2018)
 edit[isNaN(edit)] <- 0
 
 ## Capital humano educacion
+# Numerics
 
+edit$IV1R7C1[is.na(edit$IV1R7C1)] <- 0
+edit$IV1R8C1[is.na(edit$IV1R8C1)] <- 0
+
+edit$IV1R7C2[is.na(edit$IV1R7C2)] <- 0
+edit$IV1R8C2[is.na(edit$IV1R8C2)] <- 0
+
+edit <- edit %>% mutate(educadosColegio2017 = IV1R7C1 + IV1R8C1)
+edit <- edit %>% mutate(educadosColegio2017 = IV1R7C2 + IV1R8C2)
+
+edit$IV1R4C1[is.na(edit$IV1R4C1)] <- 0
+edit$IV1R5C1[is.na(edit$IV1R5C1)] <- 0
+edit$IV1R6C1[is.na(edit$IV1R6C1)] <- 0
+edit$IV1R9C1[is.na(edit$IV1R9C1)] <- 0
+
+edit$IV1R4C2[is.na(edit$IV1R4C2)] <- 0
+edit$IV1R5C2[is.na(edit$IV1R5C2)] <- 0
+edit$IV1R6C2[is.na(edit$IV1R6C2)] <- 0
+edit$IV1R9C2[is.na(edit$IV1R9C2)] <- 0
+
+edit <- edit %>% mutate(educadosSuperior2017 = IV1R4C1 + IV1R5C1 + IV1R6C1 + IV1R9C1)
+edit <- edit %>% mutate(educadosSuperior2018 = IV1R4C2 + IV1R5C2 + IV1R6C2 + IV1R9C2)
+
+edit$IV1R1C1[is.na(edit$IV1R1C1)] <- 0
+edit$IV1R2C1[is.na(edit$IV1R2C1)] <- 0
+edit$IV1R3C1[is.na(edit$IV1R3C1)] <- 0
+
+edit$IV1R1C2[is.na(edit$IV1R1C2)] <- 0
+edit$IV1R2C2[is.na(edit$IV1R2C2)] <- 0
+edit$IV1R3C2[is.na(edit$IV1R3C2)] <- 0
+
+edit <- edit %>% mutate(educadosPosgrado2017 = IV1R1C1 + IV1R2C1 + IV1R3C1)
+edit <- edit %>% mutate(educadosPosgrado2018 = IV1R1C2 + IV1R2C2 + IV1R3C2)
+
+edit$IV1R10C1[is.na(edit$IV1R10C1)] <- 0
+edit$IV1R10C2[is.na(edit$IV1R10C2)] <- 0
+
+edit <- edit %>% mutate(educadosOtro2017 = IV1R10C1)
+edit <- edit %>% mutate(educadosOtro2018 = IV1R10C2)
+
+## Ratio de ocupacion del capital humano
+# Numerics
+# Nota: la encuesta solo tiene ratios para 2018
+
+edit$IV4R4C3[is.na(edit$IV4R4C3)] <- 0
+
+edit <- edit %>% mutate(ocupadosProduccion = IV4R4C3)
+edit <- edit %>% mutate(ratioProduccion = ocupadosProduccion/totOcupados2018)
+
+edit$IV4R11C3[is.na(edit$IV4R11C3)] <- 0
+
+edit <- edit %>% mutate(ocupadosID = IV4R11C3)
+edit <- edit %>% mutate(ratioID = ocupadosID/totOcupados2018)
+
+edit$IV4R1C3[is.na(edit$IV4R1C3)] <- 0
+edit$IV4R2C3[is.na(edit$IV4R2C3)] <- 0
+edit$IV4R5C3[is.na(edit$IV4R5C3)] <- 0
+
+edit <- edit %>% mutate(ocupadosAdmin = IV4R1C3 + IV4R2C3 + IV4R5C3)
+edit <- edit %>% mutate(ratioAdmin = ocupadosAdmin/totOcupados2018)
+
+edit$IV4R3C3[is.na(edit$IV4R3C3)] <- 0
+edit <- edit %>% mutate(ocupadosMarketing = IV4R3C3)
+edit <- edit %>% mutate(ratioMarketing = ocupadosMarketing/totOcupados2018)
+
+edit[isNaN(edit)] <- 0
